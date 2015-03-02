bb5c0c7dc8c7d03dfd00c311c4ba6de2b0b9999d
Maxime <maxime.biloe@prestashop.com>
2015-03-02 14:21:00 +0100

    Fix #NM-232

--- upgrade/index.php
+++ upgrade/index.php
@@ -1,4 +1,5 @@
-{*
+<?php
+/*
 * 2007-2014 PrestaShop
 *
 * NOTICE OF LICENSE
@@ -21,23 +22,14 @@
 *  @copyright  2007-2014 PrestaShop SA
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
-*}
-
-<!-- Block links module -->
-<div id="links_block_left" class="block">
-	<h4 class="title_block">
-	{if $url}
-		<a href="{$url|escape}" title="{$title|escape}">{$title|escape}</a>
-	{else}
-		{$title|escape}
-	{/if}
-	</h4>
-	<ul class="block_content bullet">
-	{foreach from=$blocklink_links item=blocklink_link}
-		{if isset($blocklink_link.$lang)} 
-			<li><a href="{$blocklink_link.url|escape}" title="{$blocklink_link.$lang|escape}" {if $blocklink_link.newWindow} onclick="window.open(this.href);return false;"{/if}>{$blocklink_link.$lang|escape}</a></li>
-		{/if}
-	{/foreach}
-	</ul>
-</div>
-<!-- /Block links module -->
+*/
+				    	
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+						
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+						
+header("Location: ../");
+exit;
